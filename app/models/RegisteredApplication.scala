dfd286c15131465f7d3fc8ba7718852eb494ccc8
chrisjwwalker <8091191+chrisjwwalker@users.noreply.github.com>
2020-03-21 18:08:19 +0000

    GK-1: RegisteredApplication model

---
+++ app/models/RegisteredApplication.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2020 CJWW Development
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import java.util.UUID
+
+import com.cjwwdev.security.SecurityConfiguration
+import com.cjwwdev.security.obfuscation.Obfuscators
+import play.api.libs.json.{JsSuccess, JsValue, Reads}
+
+case class RegisteredApplication(name: String,
+                                 desc: String,
+                                 homeUrl: String,
+                                 redirectUrl: String,
+                                 clientType: String,
+                                 clientId: String,
+                                 clientSecret: Option[String])
+
+object RegisteredApplication extends Obfuscators with SecurityConfiguration {
+  implicit val inboundReads: Reads[RegisteredApplication] = (json: JsValue) => {
+    val clientType = json.\("clientType").as[String]
+
+    JsSuccess(RegisteredApplication(
+      json.\("name").as[String],
+      json.\("desc").as[String],
+      json.\("homeUrl").as[String],
+      json.\("redirectUrl").as[String],
+      clientType,
+      clientId = encrypt(UUID.randomUUID().toString.replace("-", "")),
+      clientSecret = clientType match {
+        case "confidential" => Some(encrypt(s"${UUID.randomUUID()}${UUID.randomUUID()}".replace("-", "")))
+        case "public" => None
+      }
+    ))
+  }
+
+  override val locale: String = this.getClass.getCanonicalName
+}
---
+++ build.sbt
@@ -0,0 +1,30 @@
+import com.typesafe.config.ConfigFactory
+import scala.util.Try
+
+val appName = "gatekeeper"
+
+val btVersion: String = Try(ConfigFactory.load().getString("version")).getOrElse("0.1.0")
+
+lazy val microservice = Project(appName, file("."))
+  .enablePlugins(PlayScala)
+  .configs(IntegrationTest)
+  .settings(PlayKeys.playDefaultPort := 5678)
+  .settings(inConfig(IntegrationTest)(Defaults.itSettings): _*)
+  .settings(
+    version                                       :=  btVersion,
+    scalaVersion                                  :=  "2.13.1",
+    organization                                  :=  "com.cjww-dev.apps",
+    resolvers                                     +=  "cjww-dev" at "https://dl.bintray.com/cjww-development/releases",
+    libraryDependencies                           ++= Seq(
+      "com.cjww-dev.libs" % "mongo-connector_2.13" % "0.2.0",
+      "com.cjww-dev.libs" % "data-defender_2.13"   % "0.5.0"
+    ),
+    fork                       in IntegrationTest :=  false,
+    unmanagedSourceDirectories in IntegrationTest :=  (baseDirectory in IntegrationTest)(base => Seq(base / "it")).value,
+    parallelExecution          in IntegrationTest :=  false,
+    fork                       in Test            :=  true,
+    testForkedParallel         in Test            :=  true,
+    parallelExecution          in Test            :=  true,
+    logBuffered                in Test            :=  false
+  )
+      
